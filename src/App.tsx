0d23443f5771bf7a5ab0147c03f3377250ce23ee
Shogo Wada <aiz4338150@gmail.com>
2022-08-13 00:16:48 +0900

    css-modules-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import { InlineStyle } from './components/InlineStyle';
+import { useState } from "react";
+import "./App.css";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
 
-export default function App () {
+export default function App() {
   return (
     <div className="App">
-      <InlineStyle/>
+      <InlineStyle />
+      <CssModules />
     </div>
   );
 }
-
