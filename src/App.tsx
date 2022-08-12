fe6dc2cffe9dc3def5524026c301e4adfa3deedc
Shogo Wada <aiz4338150@gmail.com>
2022-08-12 23:59:24 +0900

    inline-style-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import { ChildArea } from './ChildArea';
+import { InlineStyle } from './components/InlineStyle';
 
 export default function App () {
-  console.log("App")
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText: React.ChangeEventHandler<HTMLInputElement> = (e) => setText(e.target.value)
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [])
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose}/>
+      <InlineStyle/>
     </div>
   );
 }
