dfa874a8e45bef52d3b5cbc8aa7e62c251cf3dce
Shogo Wada <aiz4338150@gmail.com>
2022-08-13 01:13:15 +0900

    styled-components-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledComponents } from "./components/StyledComponents";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledComponents />
     </div>
   );
 }
