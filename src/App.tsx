4fb40e9dd454807e0042231dbbd1a1b4c97816c7
Shogo Wada <aiz4338150@gmail.com>
2022-08-13 10:48:03 +0900

    create-this-lesson-boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useCallback, useState } from 'react';
 import './App.css';
-import { ChildArea } from './ChildArea';
 
 export default function App () {
-  console.log("App")
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText: React.ChangeEventHandler<HTMLInputElement> = (e) => setText(e.target.value)
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [])
-
-  return (
+  return(
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose}/>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
-  );
+  )
 }
-
