03de2eed9e0a9cc830f22ef5bdee11c67cb4c4fd
Shogo Wada <aiz4338150@gmail.com>
2022-08-16 00:45:15 +0900

    fix boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,17 +15,19 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint', 'react'],
   root: true,
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/display-name': 'off',
-    '@typescript-eslint/triple-slash-reference': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
+    'react/prop-types': 'off', /**  jsでreactを使用する際にpropsの型定義機能を提供するライブラリ */
+    'react/react-in-jsx-scope': 'off', /** Reactをimportが必須 */
+    '@typescript-eslint/explicit-function-return-type': 'off', /** 関数の戻り値の型定義が必須 */
+    'react/display-name': 'off', /** 関数名が必須(アロー関数無効) */
+    '@typescript-eslint/triple-slash-reference': 'off', /** ///が禁止 */
+    '@typescript-eslint/no-floating-promises': 'off', /** promiseに続くエラーハンドリングが必須 */
+    '@typescript-eslint/consistent-type-definitions': 'off', /** typeかinterfaceのどちらかにする */
+    '@typescript-eslint/array-type': 'off', /** 配列の型定義をT[]かArray<>のどちらかにする */
   },
   settings: {
     react: {
